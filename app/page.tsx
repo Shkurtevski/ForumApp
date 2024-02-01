963e6b3632baea5c5ebcc623d2c699e79a5f6721
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-02-01 12:09:53 +0100

    update document

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import { NextPage } from "next";
+import Link from "next/link";
 
+const Home: NextPage = () => {
+  return (
+    <main className="banner">
+      <div className="wrapper">
+        <h3>
+          Activate community and LTV <br /> <span>on-site</span> where they
+          belong.
+        </h3>
+        <Link href="/">
+          <button className="btn-primary">Discover</button>
+        </Link>
+      </div>
+    </main>
+  );
+};
 
-export default function Home() {
-  return <main></main>;
-}
+export default Home;
