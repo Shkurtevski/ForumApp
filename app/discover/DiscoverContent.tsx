5057fb3b0dcf59fe3ab4097342d0e24bce4ef135
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2024-02-02 15:54:24 +0100

    update document

--- app/discover/DiscoverContent.tsx
+++ app/discover/DiscoverContent.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
 import Card from "./Card";
 import { CardType } from "../interfaces";
+import { HiMiniXMark } from "react-icons/hi2";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoIosSearch, IoIosArrowDown } from "react-icons/io";
 
 async function getCards() {
   const res = await fetch(" http://localhost:4000/discover", {
@@ -15,7 +18,27 @@ const DiscoverContent: NextPage = async () => {
   const cards = await getCards();
   return (
     <div className="discover-content">
-      <h1>Discover</h1>
+      <HiMiniXMark size={28} />
+      <div className="heading-wrapper">
+        <h1>Discover</h1>
+        <IoIosArrowDown size={20}/>
+      </div>
+      <div className="search-wrapper">
+        <div className="search-group-hamburger">
+          <RxHamburgerMenu size={28} />
+        </div>
+        <div className="search-group-input">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search for a channel..."
+          />
+        </div>
+        <div className="search-group-search">
+          <IoIosSearch size={23} />
+        </div>
+      </div>
       <div className="discover-content-wrapper">
         {cards.map((card: CardType) => (
           <Card key={card.id} {...card} />
